105194b2f1db89d24f8ea08288b66661e5de433d
CarlosRojas <crojasw100@gmail.com>
2021-01-28 11:50:33 -0500

    Connecting a DB using python

---
+++ EjercicioDDBB_POO.py
@@ -0,0 +1,38 @@
+import pymysql
+
+class vehiculo():
+    def __init__(self,marca,modelo):
+        self.marca = marca
+        self.modelo = modelo
+    def estado(self):
+        print("El carro es de la marca:",self.marca,"\ny es modelo:",self.modelo)
+    def conexion(self):   
+        conex= pymysql.connect(db="practica",
+        user="root",
+        password="----",
+        host="Localhost",
+        port=3306) 
+        cur= conex.cursor()
+        
+        cur.execute('''CREATE TABLE VEHICULOS(
+        ID INTEGER PRIMARY KEY AUTO_INCREMENT,
+        Marca VARCHAR(50),
+        Modelo INTEGER)''')
+        
+        datss= [(self.marca,self.modelo)]
+        cur.executemany("INSERT INTO VEHICULOS VALUES(NULL, %s,%s)",datss)
+        conex.commit()
+print("Ingrese datos del vehículo: Marca y modelo.")
+datos=input()
+dato=datos.split(",")
+mar=dato[0]
+model=int(dato[1])
+
+Carro=vehiculo(mar,model)
+Carro.conexion()
+
+
+
+
+
+
